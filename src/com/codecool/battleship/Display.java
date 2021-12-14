a0b53a756433424d0c87c4925c862411360f1bc2
Tesszike99 <teszidani02@gmail.com>
2021-12-14 09:57:13 +0100

    Fix - Input modyfied

--- src/com/codecool/battleship/Display.java
+++ src/com/codecool/battleship/Display.java
@@ -3,22 +3,40 @@ package com.codecool.battleship;
 public class Display {
 
     private String displayType;
+    private String[][] board;
+    int n;
 
     public Display (String displayType){
         this.displayType = displayType;
     }
 
+    public Display (String displayType, String[][] board){
+        this.displayType = displayType;
+        this.board = board;
+    }
+
     public void displayMenu(){
         if (displayType.equalsIgnoreCase(displayType)){
-            System.out.println("|-----------|");
-            System.out.println("| GAME MENU |");
-            System.out.println("|-----------|");
+            System.out.println("|----------------------|");
+            System.out.println("| BATTLESHIP GAME MENU |");
+            System.out.println("|----------------------|");
             System.out.println(" [1] Start game  ");
             System.out.println(" [2] High scores ");
             System.out.println(" [3] Exit");
         }
+    }
 
+    public void dispplayBoard(){
+        if (displayType.equalsIgnoreCase("board")){
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j <= n; j++) {
+                    if (board[9][3].equalsIgnoreCase("empty")){
 
+                    }
+                }
+            }
+
+        }
     }
 
 }
