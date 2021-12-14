fd108cdf7662bc13ecbe3e7f44add3264a4dcdf9
Tesszike99 <teszidani02@gmail.com>
2021-12-14 16:19:17 +0100

    Feature - coordinate input

--- src/com/codecool/battleship/Input.java
+++ src/com/codecool/battleship/Input.java
@@ -1,5 +1,6 @@
 package com.codecool.battleship;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Input {
@@ -26,6 +27,57 @@ public class Input {
         return 0;
     }
 
+    public ArrayList coordinateInputs(){
+        ArrayList<Integer> coordinateList = new ArrayList<>();
+        ArrayList<Character> abc = new ArrayList<>();
 
+        Scanner coordinateInput = new Scanner(System.in);
+        System.out.println("Coordinate input (A1 format):");
+        String coordinate = coordinateInput.nextLine();
+
+        char firstCoordinate = coordinate.charAt(0);
+        int secoundCoordinate = coordinate.charAt(1);
+        int y = secoundCoordinate - 48;
+
+        if (coordinate.length() == 3){
+            try{
+                char tenDigitsNumberSecoundHalf = coordinate.charAt(2);
+                String number2 = String.valueOf(tenDigitsNumberSecoundHalf);
+                Integer.parseInt(number2);
+                String secoundCoordinate2 = secoundCoordinate - 48 + String.valueOf(tenDigitsNumberSecoundHalf - 48);
+                y = Integer.parseInt(secoundCoordinate2);
+            }
+            catch (Exception e){
+                System.out.println("Erreoroereeelooooll");
+            }
+        }
+        else {
+        }
+
+        char c;
+        for(c = 'A'; c <= 'Z'; ++c) {
+            abc.add(c);
+        }
+
+        try{
+            int i = 0;
+            for (Character listLetter : abc) {
+                i++;
+                if (Character.toString(listLetter).equalsIgnoreCase(Character.toString(firstCoordinate)) ){
+                    coordinateList.add(i);
+                    coordinateList.add(y);
+
+                    return coordinateList;
+                }
+                else {
+                    System.out.println("INVALID!!");
+                }
+            }
+        }
+        catch (Exception e){
+
+        }
+        return  coordinateList;
+    }
 
 }
