5f1e337a7dfbea3ba4643385e6d0005a7b304eca
Tesszike99 <teszidani02@gmail.com>
2021-12-16 14:52:32 +0100

    Feature - ship placment direction validation

--- src/com/codecool/battleship/Game.java
+++ src/com/codecool/battleship/Game.java
@@ -62,16 +62,6 @@ public class Game {
         } while (input == false);
     }
 
-    /*private void createBoard() {
-        Input playerInput = new Input();
-        String askForBoardSize = playerInput.askForUser("How big board do you want to play on?");
-        int boardSize = Integer.parseInt(askForBoardSize);
-        for (int i = 0; i < 2; i++) {
-            Board board = new Board(boardSize);
-            currentPlayer.setBoard(board);
-            changePlayer();
-        }
-    } */
 
     private void placeShips() {
         for (int i = 0; i < 2; i++) {
